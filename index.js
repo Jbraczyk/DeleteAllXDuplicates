43160bf899ba78627d29f8d5f9e94c6633f4fc4e
jbraczyk <bouratchikj@gmail.com>
2022-08-30 19:02:13 +0200

    # adding On2n and numbers => array

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const deleteAllXDuplicate = (numbers) => {
+const deleteAllXDuplicate = array => {
     const sorter = (a, b) => a - b
     let elMap = {}
     let sorted = []
-
-    for (let index = 0; index < numbers.length; index++) elMap[numbers[index]] = []
-    for (el in elMap) numbers.forEach((n, index) => n === Number(el) && elMap[el].push(index))
+    
+    for (let index = 0; index < array.length; index++) elMap[array[index]] = []
+    for (el in elMap) array.forEach((n, index) => n === Number(el) && elMap[el].push(index))
     for (el in elMap) elMap[el].length >= 2 && elMap[el].forEach(e => sorted.push(e))
-
-    sorted.sort(sorter).reverse().forEach(i => numbers.splice(i, 1))
-
-    return numbers
+    
+    sorted.sort(sorter).reverse().forEach(i => array.splice(i, 1))
+    
+    return array
 }
+
+const deleteAllXDuplicateOn2n = array => array.filter(x => array.indexOf(x) === array.lastIndexOf(x))
